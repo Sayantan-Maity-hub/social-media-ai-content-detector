3e3e4e4644b2d2391ebfa2c5db85d3c51d46c47f
Sayantan-Maity-hub <maitysayantan116@gmail.com>
2026-04-06 21:48:44 +0530

    Add UI elements for image upload and caption input in Streamlit app

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -5,3 +5,12 @@ API_URL = "http://127.0.0.1:8000/analyze-post"
 st.set_page_config(page_title="ReelGuard", layout="wide")
 st.title("ReelGuard")
 st.subheader("Real-Time AI Content Detection for Social Media")
+
+col1, col2 = st.columns(2)
+with col1:
+    uploaded_image = st.file_uploader("Upload and image to analyze", type=["jpg", "jpeg", "png"])
+    caption = st.text_area("Enter caption", height = 150)
+
+    analyze_button  = st.button("Analyze Post")
+
+
